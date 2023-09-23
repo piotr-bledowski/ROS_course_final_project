8452c902ac9a44b061abf73281e28745f369942b
Piotr <piotr.bledowski.77@gmail.com>
2023-09-23 12:41:43 +0200

    Initial setup

---
+++ final_project_ws/build/my_robot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/piotr/Desktop/courses/ROS/udemy_2/ROS_course_final_project/final_project_ws/install/my_robot_description/share/my_robot_description/package.sh"
